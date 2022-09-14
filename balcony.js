40ddd7ee088325e4c0f5811a066f18231b6523dd
Shashwatsharma1147 <76209043+Shashwatsharma1147@users.noreply.github.com>
2022-09-14 17:57:33 +0530

    Add files via upload

---
+++ balcony.js
@@ -0,0 +1,45 @@
+objects = [];
+img = "";
+Status = "";
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("COCOSSD", modelLoaded);
+    document.getElementById("status").innerHTML = "status = detecting object";
+}
+function modelLoaded() {
+    console.log("model is loaded");
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, result) {
+    if (error) {
+        console.log("Error");
+    }
+    else {
+        console.log(result);
+        objects = result;
+    }
+}
+
+function preload() {
+    img = loadImage("balcony.jpg");
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if (Status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "status = Objects Detected";
+            percent = floor(objects[i].confidence*100);
+            console.log("percent = "+percent);
+            fill("red");
+            text(objects[i].label+" "+percent+"%",objects[i].x, objects[i].y);
+            noFill();
+            stroke("red");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
